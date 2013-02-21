27ab8ac12c51623bf6e6a7472b078f6757362cc7
albin <albinyewen@gmail.com>
2013-02-21 16:17:54 +0800

    Initial Commit

---
+++ CocosAmf/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  CocosAmfAppController.h
+//  CocosAmf
+//
+//  Created by Albin Zhu on 13-2-21.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ CocosAmf/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CocosAmfAppController.h
+//  CocosAmf
+//
+//  Created by Albin Zhu on 13-2-21.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
